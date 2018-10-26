401d761849ca0dda105746e7ab51f4593c024d48
Humber Aquino <humberaquino@gmail.com>
2018-10-26 10:13:38 -0300

    Initial Commit

---
+++ CamMap/CamMap.h
@@ -0,0 +1,19 @@
+//
+//  CamMap.h
+//  CamMap
+//
+//  Created by Humberto Aquino on 10/26/18.
+//  Copyright © 2018 Humberto Aquino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CamMap.
+FOUNDATION_EXPORT double CamMapVersionNumber;
+
+//! Project version string for CamMap.
+FOUNDATION_EXPORT const unsigned char CamMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CamMap/PublicHeader.h>
+
+
